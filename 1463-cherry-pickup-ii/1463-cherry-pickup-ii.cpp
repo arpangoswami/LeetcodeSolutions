232ad59e35d9b71ea4945c2e46419c2a7c1cc60d
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2022-01-08 10:06:49 +0530

    Time: 46 ms (63.71%), Space: 9.8 MB (84.88%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int dp[71][71][71];
     int m,n;
+    int sub[3] = {-1,0,1};
     int solve(vector<vector<int>> &grid,int i,int j,int k){
         if(i == m){
             return 0;
@@ -11,7 +12,22 @@ public:
         }
         if(dp[i][j][k] != -1 || dp[i][k][j] != -1)
             return dp[i][j][k];
-        return dp[i][j][k] = dp[i][k][j] = grid[i][j] + (j == k ? 0 : grid[i][k]) + max({solve(grid,i+1,j-1,k-1),solve(grid,i+1,j-1,k),solve(grid,i+1,j-1,k+1),solve(grid,i+1,j,k-1),solve(grid,i+1,j,k),solve(grid,i+1,j,k+1),solve(grid,i+1,j+1,k-1),solve(grid,i+1,j+1,k),solve(grid,i+1,j+1,k+1)});
+        // return dp[i][j][k] = dp[i][k][j] = grid[i][j] + (j == k ? 0 : grid[i][k]) + max({solve(grid,i+1,j-1,k-1),solve(grid,i+1,j-1,k),solve(grid,i+1,j-1,k+1),solve(grid,i+1,j,k-1),solve(grid,i+1,j,k),solve(grid,i+1,j,k+1),solve(grid,i+1,j+1,k-1),solve(grid,i+1,j+1,k),solve(grid,i+1,j+1,k+1)});
+        int &ans = dp[i][j][k];
+        ans = grid[i][j];
+        if(j != k){
+            ans += grid[i][k];
+        }
+        int add = 0;
+        for(int x=0;x<3;x++){
+            for(int y=0;y<3;y++){
+                add = max(add,solve(grid,i+1,j+sub[x],k+sub[y]));
+            }
+        }
+        ans += add;
+        dp[i][k][j] = ans;
+        dp[i][j][k] = ans;
+        return ans;
     }
     int cherryPickup(vector<vector<int>>& grid) {
         m = grid.size();
