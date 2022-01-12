3c76b156fd9283c632e5a7c569845840ca8011c9
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2022-01-12 11:05:31 +0530

    Time: 123 ms (59.87%), Space: 14.9 MB (65.90%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -4,15 +4,6 @@ class Solution {
         ll diffX = v1[0]-v2[0],diffY = v1[1]-v2[1];
         return diffX*diffX + diffY*diffY;
     }
-    void dfs1(int sv,vector<vector<int>> &adj_list,stack<int> &st,vector<bool> &visited){
-        visited[sv] = true;
-        for(int x:adj_list[sv]){
-            if(!visited[x]){
-                dfs1(x,adj_list,st,visited);
-            }
-        }
-        st.push(sv);
-    }
     void dfs2(int sv,vector<vector<int>> &adj_list,vector<bool> &visited,int &cnt){
         cnt++;
         visited[sv] = true;
@@ -36,30 +27,14 @@ public:
                 }
             }
         }
-        // stack<int> st;
-        // vector<bool> visited(n);
-        // for(int i=0;i<n;i++){
-        //     if(!visited[i]){
-        //         dfs1(i,adj_list,st,visited);
-        //     }
-        // }
-        // int ans = 1;
-        // fill(visited.begin(),visited.end(),false);
-        // while(!st.empty()){
-        //     int v = st.top();
-        //     st.pop();
-        //     if(!visited[v]){
-        //         int cnt = 0;
-        //         dfs2(v,adj_list,visited,cnt);
-        //         ans = max(ans,cnt);
-        //     }
-        // }
         int ans = 1;
         for(int i=0;i<n;i++){
             vector<bool> visited(n);
             int cnt = 0;
             dfs2(i,adj_list,visited,cnt);
-            ans = max(ans,cnt);
+            if(cnt > ans){
+                ans = cnt;
+            }
         }
         return ans;
     }
