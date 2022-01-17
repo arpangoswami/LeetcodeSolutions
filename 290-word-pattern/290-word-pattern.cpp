f23b731a0b69b5d722f3e47e079c0201af73af7d
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2022-01-17 12:20:43 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (18.34%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        map<char,string> mapping;
+        map<string,char> reverseMapping;
+        istringstream iss(s);
+        string word;
+        int n = pattern.size(),i = 0;
+        while(iss >> word){
+            if(i >= n){
+                return false;
+            }
+            if(mapping.count(pattern[i]) && mapping[pattern[i]] != word){
+                return false;
+            }else if(reverseMapping.count(word) && 
+                     reverseMapping[word] != pattern[i]){
+                return false;
+            }else if(!mapping.count(pattern[i]) && !reverseMapping.count(word)){
+                mapping[pattern[i]] = word;
+                reverseMapping[word] = pattern[i];
+            }
+            i++;
+        }
+        return i == n;
+    }
+};
