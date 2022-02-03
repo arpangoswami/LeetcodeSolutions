d409cf52bb4db1d7283ffc6bd026ca1873ff6b83
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2022-02-03 08:39:22 +0530

    Time: 112 ms (93.10%), Space: 6.7 MB (83.91%) - LeetHub

---
+++ 454-4sum-ii/454-4sum-ii.go
@@ -0,0 +1,17 @@
+func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
+    pairSum := make(map[int]int)
+    for _,val1 := range nums1 {
+        for _,val2 := range nums2 {
+            pairSum[val1+val2]++
+        }
+    }
+    ans := 0
+    for _,val3 := range nums3 {
+        for _,val4 := range nums4 {
+            if val, ok := pairSum[-(val3+val4)]; ok {
+                ans += val
+            }
+        }
+    }
+    return ans
+}
