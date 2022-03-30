310f71687d42b227c2cfaefbbbd566cf302e5e13
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2022-03-30 10:39:54 +0530

    Time: 352 ms (72.69%), Space: 34.2 MB (60.64%) - LeetHub

--- 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
@@ -2,19 +2,14 @@ class Dsu{
     int n;
 	vector<int> parent;
 	vector<int> height;
-	vector<int> sz;
 public:
 	Dsu(int _n){
 		n = _n;
 		parent.resize(n+1);
 		height.resize(n+1,0);
-		sz.resize(n+1,1);
 		for(int i=0;i<=n;i++){
 			parent[i] = i;
 		}
-	}
-	int getSize(int x){
-		return sz[findPar(x)];
 	}
 	int findPar(int x){
 		if(parent[x] == x){
@@ -31,10 +26,8 @@ public:
 		}
 		if(height[x_par] < height[y_par]){
 			parent[x_par] = y_par;
-			sz[y_par] += sz[x_par];
 		}else{
 			parent[y_par] = x_par;
-			sz[x_par] += sz[y_par];
 		}
 	}
 };
