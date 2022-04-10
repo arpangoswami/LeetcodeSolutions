f72c90bfa28cd75d7556b1055fcd9d59a3a1ac8c
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2022-04-10 12:44:29 +0530

    Time: 5 ms (53.39%), Space: 8.4 MB (28.60%) - LeetHub

--- 682-baseball-game/682-baseball-game.cpp
+++ 682-baseball-game/682-baseball-game.cpp
@@ -3,16 +3,16 @@ public:
     int calPoints(vector<string>& ops) {
         vector<int> record;
         for(string &x:ops){
-            if(isdigit(x[0]) || x[0] == '-'){
-                record.push_back(stoi(x));
-            }else if(x[0] == '+'){
+            if(x[0] == '+'){
                 int N = record.size();
                 record.push_back(record[N-1]+record[N-2]);
             }else if(x[0] == 'D'){
                 int N = record.size();
                 record.push_back(2*record[N-1]);
-            }else{
+            }else if(x[0] == 'C'){
                 record.pop_back();
+            }else{
+                record.push_back(stoi(x));
             }
         }
         return accumulate(record.begin(),record.end(),0);
