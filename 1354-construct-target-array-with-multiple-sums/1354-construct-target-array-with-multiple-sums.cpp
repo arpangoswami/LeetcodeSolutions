105c4cdcea434ba3b6d2dd252ab2c016361e0da1
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2022-06-24 18:08:25 +0530

    Time: 87 ms (16.78%), Space: 29.5 MB (69.93%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -2,29 +2,31 @@ typedef long long ll;
 class Solution {
 public:
     bool isPossible(vector<int>& target) {
-        priority_queue<ll> pq(target.begin(),target.end());
-        ll sum = 0;
-        if(target.size() == 1){
+        //Declaring a max heap of the array
+        priority_queue<ll> maxHeap(target.begin(),target.end());
+        int n = target.size();
+        //Checking for size of array == 1
+        if(n == 1){
             return target[0] == 1;
         }
-        for(int x:target){
-            sum += x;
-        }
-        if(pq.top() == sum){
-            return false;
-        }
-        while(pq.top() != 1){
-            ll largest = pq.top();
-            pq.pop();
-            ll nVal = 2*largest - sum;
-            if(sum - largest == 1){
+        //Calculating sum of whole array
+        ll sum = accumulate(target.begin(),target.end(),0LL);
+        while(maxHeap.top() != 1){
+            //Previous sum
+            ll largestElement = maxHeap.top();
+            maxHeap.pop();
+            //Handling a case of [1,5] where 1 has been added a bunch of times
+            if(sum - largestElement == 1){
                 return true;
             }
-            if(nVal % (sum - largest) < 1){
+            //new sum - previous sum = sum of all other elements
+            //previous sum - (new sum - previous sum)
+            ll previous = largestElement % (sum - largestElement);
+            sum = sum - largestElement + previous;
+            if(previous == 0LL || previous == largestElement){
                 return false;
             }
-            pq.push(nVal % (sum - largest));
-            sum += (nVal % (sum - largest) - largest);
+            maxHeap.push(previous);
         }
         return true;
     }
