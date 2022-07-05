fee75398f780934b8e03980cdcacfc226fb9d50b
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2022-07-05 10:48:38 +0530

    Time: 913 ms (19.77%), Space: 85.7 MB (5.02%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,22 +1,63 @@
 class Solution {
 public:
+    int find(int x,unordered_map<int,int> &parent){
+        if(parent[x] == x){
+            return x;
+        }
+        parent[x] = find(parent[x],parent);
+        return parent[x];
+    }
+    void unionRank(int x,int y,unordered_map<int,int> &parent,unordered_map<int,int> &rank){
+        int x_rep = find(x,parent),y_rep = find(y,parent);
+        if(x_rep == y_rep){
+            return;
+        }
+        if(rank[x_rep]>rank[y_rep]){
+            parent[y_rep] = x_rep;
+        }
+        else if(rank[x_rep]<rank[y_rep]){
+            parent[x_rep] = y_rep;
+        }
+        else{
+            parent[y_rep] = x_rep;
+            rank[x_rep]++;
+        }
+    }
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> hashSet;
-        for(const int &x:nums){
-            hashSet.insert(x);
+        //if(nums.size()==0) return 0;
+        unordered_map<int,int> parent;
+        unordered_map<int,int> rank;
+        for(int i=0;i<nums.size();i++){
+            parent[nums[i]] = nums[i];
+            rank[nums[i]] = 1;
         }
-        int ans = 0;
-        for(const int &x:nums){
-            if(hashSet.count(x-1)){
-                continue;
+        unordered_set<int> s;
+        for(int i=0;i<nums.size();i++){
+            if(s.find(nums[i]-1)==s.end() && s.find(nums[i]+1)==s.end()){
+                s.insert(nums[i]);
             }
-            int val = x,curr = 0;
-            while(hashSet.count(val)){
-                curr++;
-                val++;
+            else if(s.find(nums[i]-1)!=s.end() && s.find(nums[i]+1)!=s.end()){
+                unionRank(nums[i],nums[i]-1,parent,rank);
+                unionRank(nums[i],nums[i]+1,parent,rank);
+                s.insert(nums[i]);
             }
-            ans = max(ans,curr);
+            else if(s.find(nums[i]-1)==s.end() && s.find(nums[i]+1)!=s.end()){
+                unionRank(nums[i],nums[i]+1,parent,rank);
+                s.insert(nums[i]);
+            }
+            else if(s.find(nums[i]-1)!=s.end() && s.find(nums[i]+1)==s.end()){
+                unionRank(nums[i],nums[i]-1,parent,rank);
+                s.insert(nums[i]);
+            }
         }
-        return ans;
+        unordered_map<int,int> parentOf;
+        for(auto i=s.begin();i!=s.end();i++){
+            parentOf[find(*i,parent)]++;
+        }
+        int maxi = 0;
+        for(auto i=parentOf.begin();i!=parentOf.end();i++){
+            maxi = max(maxi,i->second);
+        }
+        return maxi;
     }
 };
