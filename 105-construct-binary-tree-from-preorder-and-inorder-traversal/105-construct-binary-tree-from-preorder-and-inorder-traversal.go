d3800a3d8254215489a47b72dc4ff70491193d4f
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2022-07-14 09:14:17 +0530

    Time: 7 ms (61.24%), Space: 4.4 MB (41.86%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func treeBuilder(preorder []int,inorder []int,idx *int,mp map[int]int,left,right int) *TreeNode{
+    if left > right {
+        return nil
+    }
+    inorderIdx := mp[preorder[*idx]]
+    root := TreeNode{Val: inorder[inorderIdx],Left: nil,Right: nil}
+    *idx += 1
+    root.Left = treeBuilder(preorder,inorder,idx,mp,left,inorderIdx-1)
+    root.Right = treeBuilder(preorder,inorder,idx,mp,inorderIdx+1,right)
+    return &root
+}
+func buildTree(preorder []int, inorder []int) *TreeNode {
+    n := len(inorder)
+    mp := make(map[int]int)
+    for idx,val := range inorder {
+        mp[val] = idx;
+    }
+    idx := 0
+    return treeBuilder(preorder,inorder,&idx,mp,0,n-1)
+}
