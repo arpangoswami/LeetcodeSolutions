4b3b0ce18c864fddbd6dc90404cd4e691f0d7f71
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2022-07-21 13:02:29 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (17.76%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -35,16 +35,13 @@ public:
             curr = curr->next;
         }
         ListNode *midPartHead = firstPartTail ? firstPartTail->next : head;
-        //cout<<midPartHead->val<<"\n";
         for(int i=left;i<right;++i){
             curr = curr->next;
         }
         ListNode *midPartTail = curr;
-        //cout<<midPartTail->val<<"\n";
         ListNode *thirdPartHead = curr->next;
         midPartTail->next = nullptr;
         pair<ListNode*,ListNode*> midPart = reverse(midPartHead);
-        print(midPart.first);
         (midPart.second)->next = thirdPartHead;
         if(firstPartTail){
             firstPartTail->next = midPart.first;
