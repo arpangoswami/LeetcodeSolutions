84006ebb0a38cbc905cdf22185b1ee54f52c7ac2
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2025-03-29 14:31:02 +0530

    Time: 241 ms (78.05%) | Memory: 150.6 MB (59.15%) - LeetSync

--- 3001-apply-operations-to-maximize-score/apply-operations-to-maximize-score.cpp
+++ 3001-apply-operations-to-maximize-score/apply-operations-to-maximize-score.cpp
@@ -1,7 +1,8 @@
+using ll = long long;
 class Solution {
-    const int MOD = 1e9+7;
-    int getDistinctPrimes(int n){
-        int ans = 0;
+    const ll MOD = 1e9+7;
+    ll getDistinctPrimes(ll n){
+        ll ans = 0;
         bool divBy2 = false;
         if(n % 2 == 0){
             divBy2 = true;
@@ -25,28 +26,28 @@ class Solution {
         }
         return ans;
     }
-    int getModSum(int a,int b){
+    ll getModSum(ll a,ll b){
         if(b == 0){
-            return 1;
+            return 1LL;
         }
-        int midProd = getModSum(a, b/2);
-        long long eveSum = ((long long)midProd * (long long)midProd)%MOD;
+        ll midProd = getModSum(a, b/2);
+        ll eveSum = ((ll)midProd * (ll)midProd)%MOD;
         if(b % 2 == 0){
-            return (int)(eveSum % MOD);
+            return eveSum % MOD;
         }
-        eveSum *= (long long)a;
+        eveSum *= (ll)a;
         eveSum %= MOD;
-        return (int)eveSum;
+        return eveSum;
     }
 public:
     int maximumScore(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> primeScores(n);
+        vector<ll> primeScores(n);
         for(int i=0;i<n;i++){
             primeScores[i] = getDistinctPrimes(nums[i]);
         }
-        vector<int> left(n),right(n);
-        stack<int> st;
+        vector<ll> left(n),right(n);
+        stack<ll> st;
         for(int i=0;i<n;i++){
             while(!st.empty() && primeScores[i] > primeScores[st.top()]){
                 st.pop();
@@ -72,17 +73,17 @@ public:
             }
             st.push(i);
         }
-        vector<array<int,2>> ar(n);
+        vector<array<ll,2>> ar(n);
         for(int i=0;i<n;i++){
             ar[i] = {nums[i], i};
         }
-        sort(ar.begin(), ar.end(), greater<array<int,2>>());
-        long long ans = 1;
+        sort(ar.begin(), ar.end(), greater<array<ll,2>>());
+        ll ans = 1;
         for(int i=0;i<n && k > 0;i++){
             int idx = ar[i][1];
-            long long subMath = (long long)(idx - left[idx])*(long long)(right[idx] - idx);
-            long long sub = min(subMath, (long long)k);
-            long long mul = getModSum(ar[i][0], sub);
+            ll subMath = (ll)(idx - left[idx])*(ll)(right[idx] - idx);
+            ll sub = min(subMath, (ll)k);
+            ll mul = getModSum(ar[i][0], sub);
             ans *= mul;
             ans %= MOD;
             k -= sub;
