23a741d2e53aff315bcb4bfba5eb74285fc349fe
arpangoswami <47715139+arpangoswami@users.noreply.github.com>
2025-03-31 12:19:01 +0530

    Time: 76 ms (20.25%) | Memory: 69.2 MB (34.60%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -3,22 +3,23 @@ public:
     long long putMarbles(vector<int>& v, int k) {
         priority_queue<int> maxHeap;
         priority_queue<int,vector<int>,greater<int>> minHeap;
+        long long maxSum = 0L, minSum = 0L;
         int N = v.size();
         for(int i = 0; i < N-1; i++){
+            int pairWise = v[i] + v[i+1];
+            maxSum += pairWise;
+            minSum += pairWise;
             maxHeap.push(v[i]+v[i+1]);
             minHeap.push(v[i]+v[i+1]);
             if(maxHeap.size() >= k){
+                long long maxPairSum = maxHeap.top();
+                long long minPairSum = minHeap.top();
+                maxSum -= maxPairSum;
+                minSum -= minPairSum;
                 maxHeap.pop();
                 minHeap.pop();
             }
         }
-        long long int ans1 = 0, ans2 = 0;
-        while(!maxHeap.empty()){
-            ans1 += maxHeap.top();
-            ans2 += minHeap.top();
-            maxHeap.pop();
-            minHeap.pop();
-        }
-        return ans2-ans1;
+        return minSum-maxSum;
     }
 };
