f3ab415b63be721fba3cae3f7401c4660e494ee1
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2025-04-05 17:41:25 +0530

    Time: 0 ms (100.00%) | Memory: 3.8 MB (100.00%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.go
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.go
@@ -1,15 +1,7 @@
 func subsetXORSum(nums []int) int {
-    n := len(nums)
-    maxNum := (1 << n)
-    var totalSum int
-    for i:= 0; i < maxNum; i++ {
-        currSum := 0
-        for j:= 0;j < n; j++ {
-            if (i >> j) & 1 != 0 {
-                currSum ^= nums[j]
-            }
-        }
-        totalSum += currSum
-    }
-    return totalSum
+    or := 0
+	for _, num := range nums {
+		or |= num
+	}
+	return or << (len(nums) - 1)
 }
