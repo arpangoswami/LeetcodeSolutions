5cbe5c8b4c3da0024bb22e7fd1c0cc4062f7d07b
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2025-06-29 14:20:19 +0530

    Time: 26 ms (88.41%) | Memory: 56.2 MB (7.88%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,31 +1,24 @@
 using ll = long long;
 class Solution {
     constexpr static ll MOD = 1e9+7;
-    ll getPower(ll x,ll pow){
-        ll ans = 1;
-        while(pow > 0){
-            if(pow & 1){
-                ans *= x;
-                ans %= MOD;
-            }
-            x *= x;
-            x %= MOD;
-            pow >>= 1;
-        }
-        return ans;
-    }
 public:
     int numSubseq(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
         int N = nums.size();
         ll ans = 0; 
         ll left = 0,right = N-1;
+        vector<ll> pow2(N);
+        pow2[0] = 1l;
+        for(int i=1;i<N;i++){
+            pow2[i] = pow2[i-1] * 2L;
+            pow2[i] %= MOD;
+        }
         while(left <= right){
             ll sum = nums[left] + nums[right];
             if(sum > target){
                 right--;
             }else{
-                ans += getPower(2, right - left);
+                ans += pow2[right - left];
                 ans %= MOD;
                 left++;
             }
