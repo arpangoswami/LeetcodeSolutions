e4068562f429a73ce0ad4a9b660702e5ad2c1590
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2025-07-02 02:31:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (83.10%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -3,14 +3,10 @@ public:
     int possibleStringCount(string word) {
         int ans = 1;
         int N = word.size();
-        for(int i=0;i<N;){
-            char ch = word[i];
-            int cnt = 0;
-            while(i < N && word[i] == ch){
-                i++;
-                cnt++;
+        for(int i=0;i<(N-1);i++){
+            if(word[i] == word[i+1]){
+                ans++;
             }
-            ans += (cnt - 1);
         }
         return ans;
     }
