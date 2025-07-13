54925cf7ed5515c176477308ea0d6836ffee72a6
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2025-07-13 12:25:44 +0530

    Time: 3 ms (94.85%) | Memory: 44.7 MB (74.84%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -4,7 +4,7 @@ public:
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
         int N1 = g.size(),N2 = s.size(),ans = 0;
-        for(int i=0,j=0;i<N1 && j<N2;j++){
+        for(int i=0,j=0;i<N1 && j<N2;++j){
             if(g[i] <= s[j]){
                 ans++;
                 i++;
