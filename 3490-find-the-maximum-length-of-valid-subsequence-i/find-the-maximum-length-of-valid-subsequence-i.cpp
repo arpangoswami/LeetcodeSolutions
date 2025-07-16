e6e6032eb2c276817f48e01a761a85f56e4a4af3
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2025-07-16 22:53:39 +0530

    Time: 23 ms (18.82%) | Memory: 96.2 MB (40.00%) - LeetSync

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int sameRemainder(vector<int> &nums, int rem){
+        int ans = 0;
+        for(int &x:nums){
+            if((x & 1) == rem){
+                ans++;
+            }
+        }
+        return ans;
+    }
+    int alternateRemainder(vector<int> &nums, int rem){
+        int ans = 0;
+        for(int &x:nums){
+            if((x & 1) == rem){
+                ans++;
+                rem ^= 1;
+            }
+        }
+        return ans;
+    }
+public:
+    int maximumLength(vector<int>& nums) {
+        return max(max(sameRemainder(nums, 0), sameRemainder(nums, 1)), max(alternateRemainder(nums, 0), alternateRemainder(nums, 1)));
+    }
+};
