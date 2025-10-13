f5294568c909122006abe3131bf9a3ff5805ddc3
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2025-10-13 10:24:10 +0530

    Time: 2 ms (53.90%) | Memory: 18.1 MB (19.22%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        vector<string> ans;
+        vector<int> prevFreq(26);
+        int n = words.size();
+        for(int i=0;i<n;i++){
+            vector<int> currFreq(26);
+            for(char &ch:words[i]){
+                currFreq[ch - 'a']++;
+            }
+            bool flag = true;
+            for(int i=0;i<26;i++){
+                if(currFreq[i] != prevFreq[i]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(!flag){
+                ans.push_back(words[i]);
+                for(int i=0;i<26;i++){
+                    prevFreq[i] = currFreq[i];
+                }
+            }
+        }
+        return ans;
+    }
+};
