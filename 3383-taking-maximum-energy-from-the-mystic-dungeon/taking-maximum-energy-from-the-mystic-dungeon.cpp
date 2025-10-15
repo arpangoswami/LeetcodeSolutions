9af29c8d5b53cf59558938e5457ce70867ea55d9
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2025-10-16 00:38:46 +0530

    Time: 87 ms (96.82%) | Memory: 148 MB (61.66%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,10 +1,16 @@
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
 class Solution {
 public:
     int maximumEnergy(vector<int>& energy, int k) {
         int n = energy.size();
         int ans = -1e9;
         vector<int> modSums(k);
-        for(int i=n-1;i>=0;i--){
+        for(int i=n-1;i>=0;--i){
             modSums[i % k] += energy[i];
             ans = max(ans, modSums[i%k]);
         }
