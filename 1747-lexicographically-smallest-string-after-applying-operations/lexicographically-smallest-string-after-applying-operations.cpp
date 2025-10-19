fbf49e5761f1148ac34c2fab7b3e210e6c0bf92b
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2025-10-19 23:27:51 +0530

    Time: 295 ms (30.09%) | Memory: 82.3 MB (57.08%) - LeetSync

---
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    void dfs(string &s, int a, int b, unordered_set<string> &visited,string &ans){
+        visited.insert(s);
+        ans = min(ans, s);
+        string copy = s;
+        for(int i=1;i<copy.size();i+=2){
+            char ch;
+            ch = '0' + (s[i] - '0' + a)%10;
+            copy[i] = ch;
+        }
+        if(!visited.count(copy)){
+            dfs(copy, a, b, visited, ans);
+        }
+        string copy2;
+        for(int i=b;i<s.size();i++){
+            copy2.push_back(s[i]);
+        }
+        for(int i=0;i<b;i++){
+            copy2.push_back(s[i]);
+        }
+        if(!visited.count(copy2)){
+            dfs(copy2, a, b, visited, ans);
+        }
+    }
+public:
+    string findLexSmallestString(string s, int a, int b) {
+        unordered_set<string> visited;
+        string ans = s;
+        dfs(s, a, b, visited, ans);
+        return ans;
+    }
+};
