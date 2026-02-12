06bfad56fb1e0f9178d02a30b8a8f59e10a08719
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2026-02-13 00:08:20 +0530

    Time: 207 ms (51.52%) | Memory: 25.5 MB (52.24%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -1,30 +1,64 @@
+// class Solution {
+//     bool check(vector<int> freq){
+//         int count = -1;
+//         for(int &x:freq){
+//             if(x > 0){
+//                 if(count != -1 && x != count){
+//                     return false;
+//                 }
+//                 count = x;
+//             }
+//         }
+//         return true;
+//     }
+// public:
+//     int longestBalanced(string s) {
+//         int n = s.size();
+//         int ans = 1;
+//         for(int i=0;i<n;i++){
+//             vector<int> freq(26, 0);
+//             for(int j=i;j<n;j++){
+//                 freq[s[j] - 'a']++;
+//                 if(!check(freq)){
+//                     continue;
+//                 }
+//                 ans = max(ans, j - i + 1);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
-    bool check(vector<int> freq){
-        int count = -1;
-        for(int &x:freq){
-            if(x > 0){
-                if(count != -1 && x != count){
-                    return false;
-                }
-                count = x;
-            }
+public:
+    bool checkBalanced(vector<int>& freq) {
+        int common = 0;
+        for(int i = 0; i < 26; i++) {
+            if(freq[i] == 0) continue;
+            if(common == 0)
+                common = freq[i];
+            else if(freq[i] != common)
+                return false;
         }
         return true;
     }
-public:
+
     int longestBalanced(string s) {
-        int n = s.size();
-        int ans = 1;
-        for(int i=0;i<n;i++){
+        int n = s.length();
+        int maxL = 0;
+
+        for(int i = 0; i < n; i++) {
             vector<int> freq(26, 0);
-            for(int j=i;j<n;j++){
+
+            for(int j = i; j < n; j++) {
                 freq[s[j] - 'a']++;
-                if(!check(freq)){
-                    continue;
+
+                if(checkBalanced(freq)) {
+                    maxL = max(maxL, j - i + 1);
                 }
-                ans = max(ans, j - i + 1);
             }
         }
-        return ans;
+        return maxL;        
     }
 };
