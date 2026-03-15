450670683389b59eed1c4eba24efda1f833bb7cd
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2026-03-15 14:40:06 +0530

    Time: 1 ms (65.31%) | Memory: 8.2 MB (73.67%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,30 +1,29 @@
 class Solution {
-    void recSolve(const int length,const int k, int& iteration, string &baseString, string &ans, vector<char> &letters){
-        if(baseString.length() == length){
-            iteration++;
-            if(iteration == k){
-                ans = baseString;
-            }
-            return;
-        }
-        if(iteration == k){
-            return;
-        }
-        for(char &ch:letters){
-            if(baseString.size() > 0 && ch == baseString.back()){
-                continue;
-            }
-            baseString.push_back(ch);
-            recSolve(length, k, iteration, baseString, ans, letters);
-            baseString.pop_back();
-        }
-    }
 public:
     string getHappyString(int n, int k) {
-        string ans, baseString;
-        vector<char> letters = {'a', 'b', 'c'};
-        int iteration = 0;
-        recSolve(n, k, iteration, baseString, ans, letters);
+        if (k > 3 * (1 << (n-1))){
+            return "";
+        }
+        string ans;
+        int blockSize = 1 << (n-1);
+        ans += ('a' + (k - 1)/blockSize);
+        k = (k-1)%blockSize + 1;
+        for(int i=1;i<n;i++){
+            blockSize >>= 1;
+            vector<char> choices;
+            for(char ch: {'a', 'b', 'c'}){
+                if(ch == ans.back()){
+                    continue;
+                }
+                choices.push_back(ch);
+            }
+            if(k <= blockSize){
+                ans += choices[0];
+            }else{
+                k -= blockSize;
+                ans += choices[1];
+            }
+        }
         return ans;
     }
 };
