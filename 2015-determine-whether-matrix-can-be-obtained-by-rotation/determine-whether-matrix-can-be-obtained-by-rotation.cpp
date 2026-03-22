0fc24ff91783c94da0248f204b02092ea562f750
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2026-03-22 11:10:28 +0530

    Time: 3 ms (2.82%) | Memory: 15.1 MB (9.39%) - LeetSync

---
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    vector<vector<int>> rotate(vector<vector<int>> &matrix){
+        int n = matrix.size();
+        vector<vector<int>> ans(n, vector<int>(n));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(ans[i].begin(), ans[i].end());
+        }
+        return ans;
+    }
+    bool checkEqual(vector<vector<int>> &matrix, vector<vector<int>> &matrix2){
+        int n = matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] != matrix2[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+public:
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+       vector<vector<int>> rotatedMat = rotate(mat);
+       vector<vector<int>> rotatedMat2 = rotate(rotatedMat);
+       vector<vector<int>> rotatedMat3 = rotate(rotatedMat2);
+       return checkEqual(mat, target) || checkEqual(rotatedMat, target) || checkEqual(rotatedMat2, target) || checkEqual(rotatedMat3, target);
+    }
+};
