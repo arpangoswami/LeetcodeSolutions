eb4b301d656669c91f6825f86d8014e0d6eab051
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2026-03-29 14:30:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (67.10%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        int n = s1.size(), n2 = s2.size();
-        if(n != n2){
-            return false;
+        // int n = s1.size(), n2 = s2.size();
+        // if(n != n2){
+        //     return false;
+        // }
+        // vector<int> oddFreq(26), evenFreq(26);
+        // for(int i=0;i<n;i+=2){
+        //     evenFreq[s1[i] - 'a']++;
+        //     evenFreq[s2[i] - 'a']--;
+        // }
+        // for(int i=1;i<n;i+=2){
+        //     oddFreq[s1[i] - 'a']++;
+        //     oddFreq[s2[i] - 'a']--;
+        // }
+        // for(int i=0;i<26;i++){
+        //     if(oddFreq[i] || evenFreq[i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        if(s1 == s2){
+            return true;
         }
-        vector<int> oddFreq(26), evenFreq(26);
-        for(int i=0;i<n;i+=2){
-            evenFreq[s1[i] - 'a']++;
-            evenFreq[s2[i] - 'a']--;
+        swap(s1[0], s1[2]);
+        if(s1 == s2){
+            return true;
         }
-        for(int i=1;i<n;i+=2){
-            oddFreq[s1[i] - 'a']++;
-            oddFreq[s2[i] - 'a']--;
+        swap(s1[1], s1[3]);
+        if(s1 == s2){
+            return true;
         }
-        for(int i=0;i<26;i++){
-            if(oddFreq[i] || evenFreq[i]){
-                return false;
-            }
+        swap(s1[0], s1[2]);
+        if(s1 == s2){
+            return true;
         }
-        return true;
+        return false;
     }
 };
