c520e0460be42f1e4c60fe45c6b2af531312e980
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2026-05-28 12:17:27 +0530

    Time: 80 ms (35.51%) | Memory: 24.5 MB (53.07%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        vector<int> lastLow(26, -1), firstUp(26, -1);
-        for (int i = 0; i < word.size(); i++) {
-            char c = word[i];
-            if (islower(c)) {
-                lastLow[c - 'a'] = i;
-            } else {
-                if (firstUp[c - 'A'] == -1) {
-                    firstUp[c - 'A'] = i;
-                }
+        vector<int> lastLow(26, -1), firstUpper(26, -1);
+        int n = word.size();
+        for(int i=n-1;i>=0;i--){
+            if(islower(word[i]) && lastLow[word[i] - 'a'] == -1){
+                lastLow[word[i] - 'a'] = i;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(isupper(word[i]) && firstUpper[word[i] - 'A'] == -1){
+                firstUpper[word[i] - 'A'] = i;
             }
         }
         int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            if (lastLow[i] != -1 && firstUp[i] != -1 &&
-                lastLow[i] < firstUp[i]) {
+        for(int i=0;i<26;i++){
+            if(lastLow[i] != -1 && firstUpper[i] != -1 && lastLow[i] < firstUpper[i]){
                 ans++;
             }
         }
