bbf23c72a7165cdc8d3118e775b32240e0f51a47
Arpan Goswami <47715139+arpangoswami@users.noreply.github.com>
2026-08-28 00:03:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (90.45%) - LeetSync

---
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -0,0 +1,67 @@
+// class Solution {
+// public:
+//     string lexGreaterPermutation(string s, string target) {
+//         int n = s.size();
+//         vector<int> freq1(26), freq2(26);
+//         for(int i=0;i<n;i++){
+//             freq1[s[i] - 'a']++;
+//             freq2[target[i] - 'a']++;
+//         }
+//         int st = target[0] - 'a';
+//         bool canFind = false;
+//         for(int i=st+1;i<26;i++){
+//             if(freq1[i]){
+//                 canFind = true;
+//                 break;
+//             }
+//         }
+//         if(!canFind){
+//             return "";
+//         }
+
+//     }
+// };
+
+
+class Solution {
+public:
+    string lexGreaterPermutation(string s, string target) {
+        int cnt[26] = {};
+        int mask = 0, bad = 0;
+
+        for (char ch : s) cnt[ch - 'a']++;
+        for (char ch : target) cnt[ch - 'a']--;
+
+        for (int c = 0; c < 26; c++) {
+            if (cnt[c] < 0) bad++;
+            else if (cnt[c] > 0) mask |= 1 << c;
+        }
+
+        for (int i = target.size() - 1; i >= 0; i--) {
+            int cur = target[i] - 'a';
+            cnt[cur]++;
+
+            if (cnt[cur] == 0) bad--;
+            else if (cnt[cur] == 1) mask |= 1 << cur;
+
+            if (bad > 0) continue;
+
+            int higher = mask >> (cur + 1);
+            if (!higher) continue;
+
+            int next = cur + 1 + __builtin_ctz(higher);
+            cnt[next]--;
+
+            string ans = target.substr(0, i);
+            ans += char('a' + next);
+
+            for (int c = 0; c < 26; c++) {
+                ans.append(cnt[c], char('a' + c));
+            }
+
+            return ans;
+        }
+
+        return "";
+    }
+};
